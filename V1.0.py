0407bbd28f6bfd81034c2a438fa5e32be8b3e6e3
meteorTriangle <87787306+meteorTriangle@users.noreply.github.com>
2021-08-15 23:23:37 +0800

    Update V1.0.py

--- V1.0.py
+++ V1.0.py
@@ -1,13 +1,6 @@
-# 引入 tkinter 模組
 import tkinter as tk
-
-# 建立主視窗 Frame
 window = tk.Tk()
-
-# 設定視窗標題
 window.title('I LOVE YOU')
-
-# 設定視窗大小為 300x100，視窗（左上角）在螢幕上的座標位置為 (250, 150)
 window.geometry("900x900")
 
 A=[[0,0,1,0,0,0,0,1,0,0],
@@ -20,6 +13,7 @@ A=[[0,0,1,0,0,0,0,1,0,0],
    [0,0,1,1,1,1,1,1,0,0],
    [0,0,0,1,1,1,1,0,0,0],
    [0,0,0,0,1,1,0,0,0,0],]
+
 for i in range(10):
     for j in range(10):
         if A[i][j] == 0:
@@ -28,6 +22,6 @@ for i in range(10):
         else:
             print(A[i][j])
             tk.Frame(window, width='90', height='90', bg='red', bd=2).grid(row=i, column=j)
-# 執行主程式
+            
 window.mainloop()
 
